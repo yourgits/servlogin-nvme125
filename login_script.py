9cfd48fbfe80f3d82a172ccc66cabdd471d25fc5
yourgits <project20010@proton.me>
2024-09-28 16:40:46 +0800

    修改name id值inputPassword

--- login_script.py
+++ login_script.py
@@ -36,12 +36,12 @@ async def login(username, password, panel):
         url = f'https://{panel}/login/'
         await page.goto(url)
 
-        username_input = await page.querySelector('#id_username')
+        username_input = await page.querySelector('#inputEmail')
         if username_input:
             await page.evaluate('''(input) => input.value = ""''', username_input)
 
-        await page.type('#id_username', username)
-        await page.type('#id_password', password)
+        await page.type('#inputEmail', username)
+        await page.type('#inputPassword', password)
 
         login_button = await page.querySelector('#submit')
         if login_button:
